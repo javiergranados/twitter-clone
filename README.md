853cf91f378eff5c5618e1260edc261c5f4c4920
Javier Granados <contact@jvgranados.com>
2020-11-27 00:21:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Twitter clone
+
+A twitter timeline clone built with electron
+
+<img align="left" alt="logo" src="resources/icon.ico" width="100" />
---
+++ renderer/pages/home.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Twitter clone</title>
+      </Head>
+      <div>
+        <p>
+          Hello world!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
