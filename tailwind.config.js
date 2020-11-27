a3358ffcf50ca97f2b4544cb1c3db7f72d2d95cd
Javier Granados <contact@jvgranados.com>
2020-11-27 01:38:36 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        twitterBlue: '#1DA1F2',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
