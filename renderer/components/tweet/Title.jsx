dcaf188cf3b5a04b905c3a1bf9d370b36c67be7e
Javier Granados <contact@jvgranados.com>
2020-11-30 02:53:18 +0100

    Fix 'created_at' renderer

--- renderer/components/tweet/Title.jsx
+++ renderer/components/tweet/Title.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
-import getDifferenceByMinutes from '../../utils/dates'
+import getDifference from '../../utils/dates'
 
 function Title({ user, date }) {
   return (
     <p className="mb-1 text-twitterGray">
       <span className="font-bold text-white">{user.name}</span>
       <span className="pl-1">{`@${user.screen_name}`}</span>
-      <span className="ml-2 ">{`${getDifferenceByMinutes(date)} min`}</span>
+      <span className="ml-2 ">{getDifference(date)}</span>
     </p>
   )
 }
--- renderer/utils/dates.js
+++ renderer/utils/dates.js
@@ -1,5 +1,20 @@
 import moment from 'moment'
 
-const getDifferenceByMinutes = date => moment().diff(moment(date), 'minutes')
+const getDifference = date => {
+  const now = moment()
 
-export default getDifferenceByMinutes
+  let unit = 's'
+  let diff = now.diff(moment(date), 'seconds')
+
+  if (diff > 60) {
+    unit = 'min'
+    diff = now.diff(moment(date), 'minutes')
+    if (diff > 60) {
+      unit = 'h'
+      diff = now.diff(moment(date), 'hours')
+    }
+  }
+  return `${Math.round(diff)} ${unit}`
+}
+
+export default getDifference
